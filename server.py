f857d8a5115c1384591117b327eac9e6f12e40e2
Gurveer Singh Sohal <gurveersohal433@gmail.com>
2021-01-17 18:42:46 +0530

    added server.py to recommend playlist when recommend button is clicked

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from recommend import *
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/my-link/')
+def my_link():
+	recommend('nature.jpg')
+	return 'REDIRECTING'
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
